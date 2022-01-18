82fa7b298b265f2684f2daad9c7face8ee867441
Emilio Navarro <sainfuw@aol.com>
2022-01-17 23:00:36 -0300

    new combo damage formule fix

--- src/helpers/calculateDamage.js
+++ src/helpers/calculateDamage.js
@@ -8,14 +8,24 @@ const calculateDamage = (
   allie
 ) => {
   const damage = card.abilities[0].attack;
-  const axieClass = axieState.allies[allie]?.class;
+  let axieClass = axieState.allies[allie]?.class;
   const skill = axieState.allies[allie].stats.skill;
   const morale = axieState.allies[allie].stats.morale;
-  const sameClass = axieClass === card.class ? 1.1 : 1;
-  const combo = cardsCount > 1 ? (skill * 0.55 - 12.5) / 100 + 1 : 1;
+
+  axieClass = !["Mech", "Dusk", "Dawn"].includes(axieClass)
+    ? [axieClass]
+    : axieClass === "Mech"
+    ? ["Beast", "Bug"]
+    : axieClass === "Dusk"
+    ? ["Plant", "Reptile"]
+    : axieClass === "Dawn" && ["Aquatic", "Bird"];
+
+  const sameClass = axieClass.includes(card.class) ? 1.1 : 1;
+  const combo = cardsCount > 1 ? damage * ((skill * 0.55 - 12.5) / 100) : 0;
   let critical = parseFloat(
     ((Math.sqrt(morale) * 10 + morale * 0.4 - 18) / 100 + 1).toFixed(2)
   );
+
   critical = card.name === "Ronin" && cardsCount > 2 ? critical : 1;
 
   let bonusValue = 1;
@@ -34,19 +44,21 @@ const calculateDamage = (
     (myClass === "Aquatic" && enemyClass === "Beast") ||
     (myClass === "Beast" && enemyClass === "Plant")
   ) {
-    return parseInt(damage * 1.15 * sameClass * bonusValue * critical * combo);
+    return parseInt(
+      (damage * 1.15 * sameClass + combo) * bonusValue * critical
+    );
   } else if (
     (myClass === "Plant" && enemyClass === "Beast") ||
     (myClass === "Aquatic" && enemyClass === "Plant") ||
     (myClass === "Beast" && enemyClass === "Aquatic")
   ) {
-    return parseInt(damage * 0.85 * sameClass * bonusValue * critical * combo);
+    return parseInt(damage * 0.85 * sameClass * bonusValue * critical + combo);
   } else if (
     (myClass === "Plant" && enemyClass === "Plant") ||
     (myClass === "Aquatic" && enemyClass === "Aquatic") ||
     (myClass === "Beast" && enemyClass === "Beast")
   ) {
-    return parseInt(damage * 1 * sameClass * bonusValue * critical * combo);
+    return parseInt(damage * 1 * sameClass * bonusValue * critical + combo);
   }
 };
 
